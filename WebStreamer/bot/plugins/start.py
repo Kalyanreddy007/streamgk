309586a24c20cf983bf3fd500fe6958087bf1301
Kalyan Reddy dwarakacherla <95665392+Kalyanreddy007@users.noreply.github.com>
2021-12-21 16:30:31 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -6,6 +6,6 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 @StreamBot.on_message(filters.command(['start', 'help']))
 async def start(_, m: Message):
-    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.',
+    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream LINK.JOIN OUR CHANNEL @GK_FILMS TO USE BOT. ',
                 
                   )
